b2cc6a2498c9f2165766085c3900d8569041f957
Yu-Han Lin <guesslin1986@gmail.com>
2016-05-15 18:24:43 +0800

    Update: script for generate HTML page

--- setup.sh
+++ setup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-function basic_setup() {
 mkd_dir="markdown"
 css_dir="css"
-srcs=`ls -1 $mkd_dir`
 tmpfile="tmp.md"
 
+function basicSetup() {
+srcs=`ls -1 $mkd_dir`
 for item in $srcs; do
 	echo "Cat $item into temporary file"
 	cat ./$mkd_dir/$item >> $tmpfile
 	echo -e "\n" >> $tmpfile
 done
 
-pandoc $tmpfile -f markdown -t html -H $css_dir/template.css -s -o index.html
+generateHTML
 
 rm $tmpfile
 }
 
-basic_setup
+function generateHTML() {
+echo "Producing HTML page"
+pandoc $tmpfile -f markdown -t html -H $css_dir/template.css -s -o index.html
+}
+
+basicSetup
